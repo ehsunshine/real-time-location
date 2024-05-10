3bbc165c6da298077dde8840ddb2c5ac208c5139
Ehsan Mehranvari <ehsan.mehranvari@volvocars.com>
2024-05-10 18:02:47 +0200

    Update launcher icon and the map marker

--- app/src/main/java/com/example/realtimelocation/ui/map/MapTabScreen.kt
+++ app/src/main/java/com/example/realtimelocation/ui/map/MapTabScreen.kt
@@ -1,14 +1,26 @@
 package com.example.realtimelocation.ui.map
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.realtimelocation.R
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerComposable
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.launch
@@ -27,11 +39,7 @@ fun MapTabScreen(
     ) {
         // Draw the car location on the map
         carLocation?.let { position ->
-            Marker(
-                state = MarkerState(
-                    position = position,
-                )
-            )
+            CarMarker(position)
             coroutineScope.launch {
                 cameraPositionState.animate(
                     CameraUpdateFactory.newLatLngZoom(
@@ -40,6 +48,26 @@ fun MapTabScreen(
                     ), DURATION_MS_300
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun CarMarker(position: LatLng) {
+    MarkerComposable(
+        state = MarkerState(
+            position = position,
+        ),
+    ) {
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .background(Color.Black, RoundedCornerShape(size = 100.dp))
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            // Draw the car icon
+            Icon(painter = painterResource(id = R.drawable.car), contentDescription = "Car", tint = Color.White)
         }
     }
 }
