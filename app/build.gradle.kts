87444153ca49d276dfcc2b19a297f57eb05d1d77
Ehsan Mehranvari <ehsan.mehranvari@volvocars.com>
2024-05-16 14:11:17 +0200

    Add Gson dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ android {
 
 dependencies {
 
+    implementation(libs.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
