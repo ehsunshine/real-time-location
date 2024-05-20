11e4b38948ce32b3066daa410a24d254d00f9502
Ehsan Mehranvari <ehsan.mehranvari@volvocars.com>
2024-05-20 21:09:46 +0200

    Update CarLocationRepository.kt

--- app/src/main/java/com/example/realtimelocation/data/location/CarLocationRepository.kt
+++ app/src/main/java/com/example/realtimelocation/data/location/CarLocationRepository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 internal class CarLocationRepository(
-    private val serverAddress: Uri,
+    serverAddress: Uri,
 ) {
 
     fun get(): Flow<Location> = flowOf()
