0c3f2514295baa30a6340b62bfb5a6fbe060ea6a
Ehsan Mehranvari <ehsunshine@gmail.com>
2024-05-24 16:18:05 +0200

    Get real-time location from the gRPC service (#2)
    * Get real-time location from the gRPC service
    
    * Rename the service name
    
    * Provide carID

--- client/app/src/main/java/com/example/realtimelocation/data/location/CarLocationRepository.kt
+++ client/app/src/main/java/com/example/realtimelocation/data/location/CarLocationRepository.kt
@@ -2,16 +2,44 @@ package com.example.realtimelocation.data.location
 
 import android.location.Location
 import android.net.Uri
+import com.google.gson.Gson
 import com.google.gson.stream.JsonReader
+import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.InputStreamReader
 
 internal class CarLocationRepository(
     serverAddress: Uri,
 ) {
+    private val channel = ManagedChannelBuilder
+        .forAddress(serverAddress.host, serverAddress.port)
+        .usePlaintext()
+        .defaultServiceConfig(readJsonFileAsMap())
+        .enableRetry()
+        .build()
 
-    fun get(): Flow<Location> {
-        return flowOf()
+    private fun readJsonFileAsMap(): Map<String, Any> {
+        return Gson().fromJson(
+            JsonReader(
+                InputStreamReader(
+                    this::class.java.getResourceAsStream(
+                        "retry_config.json"
+                    )
+                )
+            ), Map::class.java
+        ) as Map<String, Any>
     }
 
+    fun get(): Flow<Location> = LocationServiceGrpcKt.LocationServiceCoroutineStub(channel)
+        .withWaitForReady()
+        .getLocation(locationRequest { this.carId = "VIN12345" })
+        .map {
+            Location("gps").apply {
+                longitude = it.longitude
+                latitude = it.latitude
+            }
+        }
+        .catch { it.printStackTrace() }
 }
